3cc3c4ab941700449d372b5bb0defde9c1f79ef0
Gerard Press <killerabs@gmail.com>
2023-06-04 16:36:42 +0100

    working contact form

---
+++ js/email.js
@@ -0,0 +1,24 @@
+// (function () {
+//   emailjs.init("EZTggEHkDW0QlCThM");
+// })();
+
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const serviceID = "service_lvueeix";
+  const templateID = "template_18tk5j9";
+
+  // send the email here
+  emailjs.sendForm(serviceID, templateID, this).then(
+    (response) => {
+      console.log("SUCCESS!", response.status, response.text);
+      alert("SUCCESS!");
+    },
+    (error) => {
+      console.log("FAILED...", error);
+      alert("FAILED...", error);
+    }
+  );
+
+  document.getElementById("myForm").reset();
+});
